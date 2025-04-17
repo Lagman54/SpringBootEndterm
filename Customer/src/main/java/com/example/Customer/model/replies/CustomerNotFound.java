6500d6c74f974b24cab9d87367a4df67f92ace80
Lagman54 <aibar.kydiarxan@gmail.com>
2025-04-18 01:38:51 +0500

    upd models.replies.CustomerNotFound in Customer & Order

--- Customer/src/main/java/com/example/Customer/model/replies/CustomerNotFound.java
+++ Customer/src/main/java/com/example/Customer/model/replies/CustomerNotFound.java
@@ -1,3 +1,3 @@
 package com.example.Customer.model.replies;
 
-public record CustomerNotFound(Long customerId) implements CustomerPaymentResult {}
+public record CustomerNotFound(Long customerId, Long orderId) implements CustomerPaymentResult {}
--- Order/src/main/java/com/example/Order/model/replies/CustomerNotFound.java
+++ Order/src/main/java/com/example/Order/model/replies/CustomerNotFound.java
@@ -1,3 +1,3 @@
 package com.example.Order.model.replies;
 
-public record CustomerNotFound(Long customerId) implements CustomerPaymentResult {}
+public record CustomerNotFound(Long customerId, Long orderId) implements CustomerPaymentResult {}
